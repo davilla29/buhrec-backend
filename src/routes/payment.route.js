2616478f758775d95d60bb8f542f3157a91847b0
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-02-26 19:16:43 +0100

    update payment controller to use axios call and remove webhook for now from controller and route

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -7,7 +7,5 @@ const router = express.Router();
 // Flutterwave redirect callback
 router.get("/flutterwave/callback", PaymentController.flutterwaveCallback);
 
-// (Recommended) webhook for reliability
-router.post("/flutterwave/webhook", PaymentController.flutterwaveWebhook);
 
 export default router;
