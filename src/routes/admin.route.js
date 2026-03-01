07fefdda2a181bc6fe3a97ac35e24dd7d96c2340
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-03-01 10:03:56 +0100

    update admin route to work properly

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -21,13 +21,28 @@ router.get(
 );
 
 // Get assignments list for the tabs
-router.get("/assignments/list", AdminController.getAssignmentsList);
+router.get(
+  "/assignments/list",
+  verifyToken,
+  isAdmin,
+  AdminController.getAssignmentsList,
+);
 
 // Dashboard stats
-router.get("/dashboard", AdminController.getDashboardStats);
+router.get(
+  "/dashboard",
+  verifyToken,
+  isAdmin,
+  AdminController.getDashboardStats,
+);
 
 // Get applicant payments list (Supports ?filter=successful or ?filter=pending)
-router.get("/payments/list", AdminController.getPaymentsList);
+router.get(
+  "/payments/list",
+  verifyToken,
+  isAdmin,
+  AdminController.getPaymentsList,
+);
 
 // Get reviewers by ID
 router.get(
@@ -35,6 +50,22 @@ router.get(
   verifyToken,
   isAdmin,
   AdminController.getReviewerById,
+);
+
+// Get all details for a specific proposal for the admin view
+router.get(
+  "/proposals/:proposalId/details",
+  verifyToken,
+  isAdmin,
+  AdminController.getAdminProposalDetails,
+);
+
+// Unassign an active assignment 
+router.put(
+  "/assignments/:assignmentId/unassign",
+  verifyToken,
+  isAdmin,
+  AdminController.unassignAssignment,
 );
 
 router.post(
